4569836e65a6a32053c17251fe5277715976c7e5
sschuepbach <sebastian.schuepbach@unibas.ch>
2015-09-02 09:51:30 +0200

    Settings are now in external file.

--- src/main/java/linked/swissbib/ch/App.java
+++ src/main/java/linked/swissbib/ch/App.java
@@ -1,5 +1,9 @@
 package linked.swissbib.ch;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 /**
  * @author Sebastian Schüpbach, project swissbib, Basel
@@ -8,32 +12,43 @@ public class App {
 
     public static void main (String[] args) {
 
-        // Connection details for Virtuoso server
-        String repoHost = "";
-        String repoUser = "";
-        String repoPwd = "";
-
-        // Connection details for Elasticsearch cluster
-        // String[] esNodes = {""};
-        // String esClustername = "";
-        String index = "";
-        String[] types = {"bibliographicResource", "document", "person", "organization", "work", "item"};
-        short esBulkSize = 100;
-        short outDirSize = 300;
-        String rootOutDir = "";
-        String outFilePrefix = "esbulk";
-
+        Properties properties = new Properties();
+        BufferedInputStream stream;
+        try {
+            stream = new BufferedInputStream(new FileInputStream(args[0]));
+            properties.load(stream);
+            stream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         // Set up pipe
-        // ESBulkWritable esIndex = new ESBulkIndexer(esNodes, esClustername, esBulkSize);
-        ESBulkWritable esIndex = new ESBulkWriter(esBulkSize, outDirSize, rootOutDir, outFilePrefix);
-        esIndex.connect();
-        BulkJSONLDWriter jsonldWriter = new BulkJSONLDWriter(esIndex, index);
-        GetRdfStatements rdfStatements = new GetRdfStatements(repoHost, repoUser, repoPwd, jsonldWriter);
+        ESBulkWritable esBulkDump;
+        if (properties.getProperty("writeToFile").equals("true")) {
+            esBulkDump = new ESBulkWriter(
+                    Short.parseShort(properties.getProperty("esBulkSize")),
+                    Short.parseShort(properties.getProperty("outDirSize")),
+                    properties.getProperty("rootOutDir"),
+                    properties.getProperty("outFilePrefix"));
+        } else {
+            esBulkDump = new ESBulkIndexer(
+                    properties.getProperty("esNodes").split("#"),
+                    properties.getProperty("esClustername"),
+                    Short.parseShort(properties.getProperty("esBulkSize")));
+        }
+        esBulkDump.connect();
+        BulkJSONLDWriter jsonldWriter = new BulkJSONLDWriter(
+                esBulkDump,
+                properties.getProperty("index"));
+        GetRdfStatements rdfStatements = new GetRdfStatements(
+                properties.getProperty("repoHost"),
+                properties.getProperty("repoUser"),
+                properties.getProperty("repoPwd"),
+                jsonldWriter);
 
         // Kick off workflow
-        for (String type: types) rdfStatements.getSubjects(type);
-        esIndex.close();
+        for (String type: properties.getProperty("types").split("#")) rdfStatements.getSubjects(type);
+        esBulkDump.close();
         rdfStatements.close();
 
     }
