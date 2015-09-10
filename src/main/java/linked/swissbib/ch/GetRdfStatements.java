0b599b4b5dc04f8b3510e1fe80f5eaca0f31f752
FB <fxbensmann@users.noreply.github.com>
2015-09-10 17:21:12 +0200

    Added alternative getSubject method in GetRdfStatements

--- src/main/java/linked/swissbib/ch/GetRdfStatements.java
+++ src/main/java/linked/swissbib/ch/GetRdfStatements.java
@@ -1,5 +1,13 @@
 package linked.swissbib.ch;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.logging.log4j.LogManager;
@@ -7,19 +15,24 @@ import org.apache.logging.log4j.Logger;
 import org.openrdf.model.Resource;
 import org.openrdf.model.Statement;
 import org.openrdf.model.URI;
-import org.openrdf.model.impl.StatementImpl;
-import org.openrdf.model.impl.URIImpl;
 import org.openrdf.query.*;
 import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.RepositoryException;
 
 import org.openrdf.repository.sparql.SPARQLRepository;
+import org.openrdf.model.Value;
+import org.openrdf.model.impl.StatementImpl;
+import org.openrdf.model.impl.URIImpl;
+import org.openrdf.rio.RDFFormat;
 import org.openrdf.rio.RDFHandlerException;
-
-import java.util.*;
+import org.openrdf.rio.RDFParseException;
+import org.openrdf.rio.RDFParser;
+import org.openrdf.rio.Rio;
+import org.openrdf.rio.helpers.RDFHandlerBase;
 
 /**
  * Get rdf statements from a remote Virtuoso server.
+ *
  * @author Sebastian Schüpbach, project swissbib, Basel
  */
 public class GetRdfStatements {
@@ -31,7 +44,6 @@ public class GetRdfStatements {
     String type;
     String id = null;
     BulkJSONLDWriter jsonldWriter;
-
 
     GetRdfStatements(String repoUrl, String repoUser, String repoPwd, BulkJSONLDWriter jsonldWriter) {
         this.jsonldWriter = jsonldWriter;
@@ -49,7 +61,6 @@ public class GetRdfStatements {
 
     }
 
-
     public void getSubjects(String type) {
         this.type = type;
         try {
@@ -65,7 +76,39 @@ public class GetRdfStatements {
             logger.error(e.getStackTrace());
         }
     }
+    
 
+    public void getSubjects(File linkFile) {
+        RDFParser rdfParser = Rio.createParser(RDFFormat.NTRIPLES);
+        rdfParser.setRDFHandler(new RDFHandlerBase() {
+            @Override
+            public void handleStatement(Statement st) {
+                Value object = st.getObject();
+                if (!(object instanceof org.openrdf.model.Literal)) {
+                    String subject = object.stringValue();
+                    GetRdfStatements.this.id = subject.substring(subject.lastIndexOf("/") + 1);
+                    GetRdfStatements.this.getSubjectStatements(subject);
+                }
+            }
+
+        });
+        InputStream inStream;
+        try {
+            inStream = new FileInputStream(linkFile);
+            rdfParser.parse(inStream, "");
+            inStream.close();
+        } catch (FileNotFoundException e ){
+            logger.error(e.getStackTrace());
+        } catch (IOException | RDFParseException | RDFHandlerException e) {
+            logger.error(e.getStackTrace());
+        }
+    }
+
+    
+        
+
+       
+    
 
     public static String queryBuilder(String type) {
         Map<String, String> m = new HashMap<>();
@@ -77,7 +120,6 @@ public class GetRdfStatements {
         m.put("item", "http://bibframe.org/vocab/HeldItem");
         return "SELECT DISTINCT ?s WHERE {?s <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <" + m.get(type) + "> }";
     }
-
 
     public void getSubjectStatements(String subject) {
         String query = "SELECT ?s ?p ?o WHERE { ?s ?p ?o. FILTER (?s = <" + subject + ">)}";
@@ -101,7 +143,6 @@ public class GetRdfStatements {
             logger.error(e.getMessage() + "\nQuery: " + query);
         }
     }
-
 
     public void close() {
         try {
