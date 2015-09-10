0a9b025f666337404c60625c534dd463dee241bc
FB <fxbensmann@users.noreply.github.com>
2015-09-10 17:23:28 +0200

    Adapted App for use of a link file

--- src/main/java/linked/swissbib/ch/App.java
+++ src/main/java/linked/swissbib/ch/App.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
@@ -16,7 +17,7 @@ public class App {
 
     private static final Logger logger = LogManager.getLogger("global");
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         Properties properties = new Properties();
         BufferedInputStream stream;
@@ -58,10 +59,21 @@ public class App {
                 jsonldWriter);
 
         // Kick off workflow
-        for (String type: getSetting(properties, "types").split("#")) rdfStatements.getSubjects(type);
-        esBulkDump.close();
-        rdfStatements.close();
-
+        //   load subject IDs from link file
+        if (getSetting(properties, "useLinkFile").equals("true")) {
+            File linkFile = new File( getSetting(properties, "linkFile") );
+            rdfStatements.getSubjects(linkFile);
+            esBulkDump.close();
+            rdfStatements.close();
+        } 
+        //   load subject IDs from triplestore
+        else {
+            for (String type : getSetting(properties, "types").split("#")) {
+                rdfStatements.getSubjects(type);
+            }
+            esBulkDump.close();
+            rdfStatements.close();
+        }
     }
 
     public static String getSetting(Properties properties, String val) {
